2456e1dca5a3dcbdc631992274bb41844120a2e5
Chaytan Arora <chaytanarora@gmail.com>
2022-11-03 23:57:45 +0530

    added password hashing using bcrypt

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,29 +2,31 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { registerValidation } = require("../validation");
+const bcrypt = require("bcryptjs");
 
 router.post("/register", async (req, res) => {
 	// validate user data
 	const { error } = registerValidation(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
 
-	if (error) {
-		res.status(400).send(error.details[0].message);
-	} else {
-		const user = new User({
-			name: req.body.name,
-			email: req.body.email,
-			password: req.body.password,
-		});
+	// hash password
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-		try {
-			const savedUser = await user.save();
-			res.send(savedUser);
-		} catch (error) {
-			if (error.code === 11000) {
-				res.status(400).send("Email already exists");
-			} else {
-				res.status(400).send(error);
-			}
+	const user = new User({
+		name: req.body.name,
+		email: req.body.email,
+		password: hashedPassword,
+	});
+
+	try {
+		await user.save();
+		res.json({ id: user._id, message: "User successfully added" });
+	} catch (error) {
+		if (error.code === 11000) {
+			res.status(400).send("Email already exists");
+		} else {
+			res.status(400).send(error);
 		}
 	}
 });
